4c005025df886f6971426df4d30a46f7fd7d5d42
vladimirosic <121907693+vladimirosic@users.noreply.github.com>
2023-01-20 11:25:28 +0100

    Add files via upload

--- Quelle.cpp
+++ Quelle.cpp
@@ -1,119 +1,220 @@
-//: C10:MessengerDemo.cpp
+//	https://www.arndt-bruenner.de/mathe/10/parabeltangente.htm#:~:text=Somit%20ergibt%20sich%20f%C3%BCr%20die%20Tangente%20an%20die,jeden%20Punkt%20x%20durch%202ax%20%2B%20b%20gegeben.
+
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <math.h>
-using namespace std;
+#include <iterator>
+#include <initializer_list>
 
-class Point { // Un mesenger
+class Point
+{
+private:
+	float mX, mY;
 public:
-	float x, y, z; // Puisque c’est juste un transporteur
-	Point() { x = 0.0; y = 0.0; z = 0.0; }
-	Point(float xi, float yi, float zi) : x(xi), y(yi), z(zi) {}
-	Point(const Point& p) : x(p.x), y(p.y), z(p.z) {}
-	Point& operator=(const Point rhs) {
-		x = rhs.x;
-		y = rhs.y;
-		z = rhs.z;
+	Point() :
+		mX{ 0 },
+		mY{ 0 }
+	{}
+	Point(float x, float y) :
+		mX{ x },
+		mY{ y }
+	{}
+	Point(Point const& p) :
+		mX{ p.mX },
+		mY{ p.mY }
+	{}
+	Point& operator=(Point const& rhs) {
+		mX = rhs.mX;
+		mY = rhs.mY;
 		return *this;
 	}
-	friend ostream&
-		operator<<(ostream& os, const Point& p) {
-		return os << " x=" << p.x << " y=" << p.y << " z=" << p.z;
+	float x() const
+	{
+		return mX;
+	}
+	float y() const
+	{
+		return mY;
+	}
+	void set_x(float xx)
+	{
+		mX = xx;
+	}
+	void set_y(float yy)
+	{
+		mY = yy;
+	}
+	friend std::ostream&
+		operator<<(std::ostream& os, Point const& p) {
+		return os << "x = " << p.mX << " y = " << p.mY;
 	}
 };
-
-class Vector { // Mathematical vector
+class Kurve : public Point {
 public:
-	float magnitude, angleAlfa, angleBeta, angleGama;
-	Vector(float m, float angleAlf, float angleBet, float angleGam) : magnitude(m), angleAlfa(angleAlf), angleBeta(angleBet)
-	,angleGama (angleGam) {}
-};
-class Line  {	//?!?
-	Point po1, po2;
+	std::vector<Point> kur;
+
+
 public:
-	Line(Point poi1, Point poi2) : po1{ poi1 }, po2{ poi2 }{}
-	float coin() { return atan((po2.y - po1.y) / (po2.x - po1.x)); }
+	Kurve(std::initializer_list<Point> liste) : kur{ liste } {}
+	Kurve(Point p) {
+		kur.push_back(p);
+	}
 
-	float x2_x1() const {
-		return (po2.x - po1.x);
+	std::vector<Point>& get_kur() {
+		return kur;
 	}
-	float y2_y1() const {
-		return (po2.y - po1.y);
-	}
-	float x3y4_y3x4( Line li2) const {
-		return (li2.po1.x * li2.po2.y - li2.po1.y * li2.po2.x);
-	}
-	float x1y2_y1x2() const{
-		return (po1.x * po2.y - po1.y * po2.x);
-	}
-	float dénominateur(Line li2) const{ return (x2_x1() * y4_y3(li2) - y2_y1() * x4_x3(li2)); }
-	
-	float x4_x3(Line lin2) const { return (lin2.po2.x - lin2.po1.x); }
-	float y4_y3(Line lin2) const{ return (lin2.po2.y - lin2.po1.y); }
-	Point& intersection(Line li2);
 };
-Point po;
-Point& Line::intersection(Line li2) {
-	
-	po.x = (x2_x1() * x3y4_y3x4(li2) - x4_x3(li2) * x1y2_y1x2())
-		/ dénominateur(li2);
-	po.y = (y2_y1() * x3y4_y3x4(li2) - y4_y3(li2) * x1y2_y1x2())
-		/ dénominateur(li2);
-	return po;
-}
 
-class Space {
+
+class Segment
+{
+private:
+	Point mA, mB;
 public:
-	static Point translate(Point p, Vector v) {
-		// Copy-constructeur empêche la modification de l’original,
-		// A dummy calculation:
-		p.x += v.magnitude * cos(v.angleAlfa);
-		p.y += v.magnitude * cos(v.angleBeta);
-		p.z += v.magnitude * cos(v.angleGama);
-		return p;
+	Segment(Point A, Point B) :
+		mA{ A }, mB{ B }
+	{}
+	float A() const		// Coefficient A: y = Ax + B
+	{
+		return (mA.y() - mB.y()) / (mA.x() - mB.x());
+		// source: https://calculis.net/droite
+					// A completer
+	}
+	float B() const		// Coefficient B: y = Ax + B
+	{
+		return mA.y() - A() * mA.x();
+		// source: https://calculis.net/droite
+					// A completer
+	}
+	Point intersection(Segment const& other)
+	{
+		float x = (other.B() - B()) / (A() - other.A());
+		// source: https://calculis.net/intersection
+		// A completer
+
+		// A completer
+		return Point{ x , A() * x + B() };
+	}
+	std::string equation() const
+	{
+		return "y = " + std::to_string(A()) + "x + " + std::to_string(B());
 	}
 };
-
 int main() {
-	Point A(1, 2, 3);
-	Point B = Space::translate(A, Vector(11.1e0, 47.2e0,33.5e0, 23.4e0));
-	Point C = Space::translate(B, Vector(5.5e0, 8.8e0 ,55.7e0, 6.7e0));
-	Point D(B);
-	cout << "A: " << A << " B: " << B << " C: " << C << " D: " << D << endl;
-	A = B;
-	cout << "A après A = B: " << A << endl;
-	vector<Point> wer;
-	wer.push_back(A);
-	wer.push_back(B);
-	wer.push_back(C);
-	wer.push_back(D);
-	vector<Point>::iterator it = wer.begin();
-	while (it != wer.end()) {
-		cout << it->x << endl;
-		++it;
+	Point poiA(0, 0);
+	Point poiB(2, pow(2, 2));
+
+
+	Kurve parabole({ 0,0 });
+	for (float i = 0; i < 100; i++)
+		parabole.kur.push_back({ i * 0.1f,pow(i * 0.1f,2.0f) });
+
+	float xQ, yQ;
+	std::cout << "Veuillez calculer le point à partir duquel la tangente est calculée sur la parabole:   " << std::endl;
+	std::cout << "S’il vous plaît x: ";
+	std::cin >> xQ;
+
+	std::cout << std::endl;
+	std::cout << "S’il vous plaît y: ";
+	std::cin >> yQ;
+	float aa, bb, cc;
+	std::cout << std::endl;
+	std::cout << "a * x^2 + b * x + c: S'il vous plait a: " << std::endl;
+	std::cin >> aa;
+	std::cout << std::endl;
+	std::cout << "a * x^2 + b * x + c: S'il vous plait b: " << std::endl;
+	std::cin >> bb;
+	std::cout << std::endl;
+	std::cout << "a * x^2 + b * x + c: S'il vous plait c: " << std::endl;
+	std::cin >> cc;
+	Kurve parabole_Bsp_1({ 0.0f,0.0f });
+	for (float i = -30.0f; i < 100; i++) {
+		parabole_Bsp_1.kur.push_back({ i * 0.1f, aa * pow(i * 0.1f,2.0f) + bb * i * 0.1f + cc });
 	}
-	
-	Point pA(3.5, 4, 0);
-	Point pB(-4, 2, 0);
-	Point pC(2, -2, 0);
-	Point pD(-2.0, 5, 0);
-	cout << "test pA : " << pA << endl;
 
-	Line gérades1(pA, pB);
-	Line gérades2(pC, pD);
-	
-	cout <<  gérades1.intersection( gérades2) << endl;	//	intersection_d_deux_lignes_droites
-	Point pP(7.5,-3, 0);
-	Point pQ(-5.5, 4, 0);
-	Point pR(5, 3, 0);
-	Point pS(-1, -6, 0);
+	// Calculer la tangente , Point Q connu Q(-1,-1), La tangente a une forme y = Ax + B , Parabole f(x) = ax^2 + bx +c
+	// Point de contact T(xT,yT) , yT = A*xT + B, A est tangente de l’angle de la tangente A = f'(x) = 2ax + b
 
-	Line gérades3(pP, pQ);
-	Line gérades4(pR, pS);
 
-	cout << gérades3.intersection(gérades4);	//	intersection_d_deux_lignes_droites
 
-} ///~
+	// Parabel yT = aaxT^2 + bbxT +cc  ; Tangente yT = (2aaxT +bb) xT + B
 
+
+
+
+	//  https://www.youtube.com/watch?v=ql_w5paclOs
+
+	// f(x)Tangente = f(x)Parabel
+	// aax^2 +bx + c = AxO + B = (2 * aa * x + bb) * x + B
+	// aa * x^2  + bb * x * xQ + cc = 2 * aa * x^2 + bb * x + B 
+	// -aa *  x^2 + (bb * xQ - bb ) * x + 2 * xQ - yQ + 1 = 0
+
+
+
+	float a = -aa;
+
+	float b = bb * xQ - bb;
+
+	float c = 2 * xQ - yQ + 1;
+
+	float xT1 = ((-1) * b + std::sqrt(pow(b, 2.0f) - 4 * a * c)) / (2 * a);
+
+	float xT2 = ((-1) * b - std::sqrt(pow(b, 2.0f) - 4 * a * c)) / (2 * a);
+
+	float A1 = -6.0f * xT1 + 2;
+	float A2 = -6.0f * xT2 + 2;
+	float B1 = yQ - A1 * xQ;
+	float B2 = yQ - A2 * xQ;
+
+	float yT1 = A1 * xT1 + B1;
+	float yT2 = A2 * xT2 + B2;
+
+	Point Q(xQ, yQ);
+	Point T1(xT1, yT1);
+	Point T2(xT2, yT2);
+	Segment tangente1(Q, T1);
+	std::cout << tangente1.equation() << std::endl;
+	Segment tangente2(Q, T2);
+	std::cout << tangente2.equation() << std::endl;
+
+
+
+	std::cout << "Le point de contact de la tangente1 et de la parabole est "
+		<< "T1(" << xT1 << ", " << yT1 << ")" << std::endl;
+	std::cout << "Le point de contact de la tangente1 et de la parabole est "
+		<< "T2(" << xT2 << ", " << yT2 << ")" << std::endl;
+
+
+	auto it = parabole_Bsp_1.kur.begin();
+	while (it != parabole_Bsp_1.kur.end()) {
+		std::cout << "parabole(x, y) " << *it++ << std::endl;
+
+	}
+	std::cout << "Hiperbole " << std::endl;
+	Kurve hiperbole({ 0,0 });
+	for (float i = 0; i < 100; i++)
+		hiperbole.kur.push_back({ i * 0.1f,pow(i * 0.1f,.5f) });
+
+
+
+
+
+	auto it_h = hiperbole.kur.begin();
+	while (it_h != hiperbole.kur.end()) {
+		std::cout << *it_h++ << std::endl;
+
+	}
+
+
+	Point pA(3.5f, 4.0f);
+	Point pB(-4.0f, 2.0f);
+	Point pC(2.0f, -2.0f);
+	Point pD(-2.0f, 5.0f);
+	std::cout << "Point d'intersection des deux lignes droites: " << std::endl;
+	Segment Line1(pA, pB);
+	std::cout << Line1.equation() << std::endl;
+	Segment Line2(pC, pD);
+	std::cout << Line2.equation() << std::endl;
+
+	std::cout << Line1.intersection(Line2);	// Point d'intersection des deux lignes droites	
+}
